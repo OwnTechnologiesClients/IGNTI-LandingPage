c4997b7a9b6c031313455dc3f12beba62f502c53
Pawan Kumar <pyadav.workmail@gmail.com>
2023-10-02 02:15:38 +0530

    navbar section with header img done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,29 +3,15 @@ import { useState } from 'react';
 import { Grid, Link, Typography, Box } from '@mui/material'
 import './Header.css';
 
-//import bannerImg from '/img/head_logo.jpg'
-///Users/pawanyadav/Documents/OwnTechnologyClient/ignti-landing-page/public/img/head_logo.jpg
-
-// import PowerSlap from "././video/Background_vdo.mov"
-
-
 
 const Header = () => {
 
     return (
         <>
-            <Grid
-                height={{ lg: '30vh' }}
-                >
 
-                <div className='header_img'>
-                    <img src="/img/head_logo.jpg" alt="pubg" />
-                </div>
-
-
-            </Grid>
-
-
+            <div className='header_img'>
+                <img src="/img/head-logo.jpg" alt="header-logo" />
+            </div>
 
         </>
     )
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,98 +1,69 @@
 import React from 'react'
 import "./navbar.css"
-import { FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from 'react-icons/fa'
 import { Button, Grid, Link, Typography, } from '@mui/material'
 import { NavLink } from "react-router-dom"
 
-const SocialMedia = ({ icon, alt }) => (
-    <Grid
-        ml={3}
-        width={40}
-        height={40}
-        bgcolor="#7C81E2"
-        sx={{ borderRadius: 20 }}
-        alignItems='center'
-        display='flex'
-        justifyContent='center'>
-
-        <div className='social-media-img-size'>
-            <img src={icon} alt={alt} />
-        </div>
-
-    </Grid>
-)
 
 const Navbar = () => {
     return (
         <>
-            <Grid
-
-                id="downloadApp"
-            // container
-            // direction={{ lg: 'row', md: 'row', sm: 'column', xs: 'column' }}
-            >
-
-                <nav className='main-nav'>
-
-                    {/* --------------- Menu Links ---------------------- */}
-                    <div className='menu-link'>
-                        <ul>
-                            <li>
-                                <NavLink to='/home'>Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/courses'>Courses</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/franchise'>Franchise</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/contactus'>Contact Us</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/aboutus'>About Us</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/gallery'>Gallery</NavLink>
-                            </li>
-
-                            <li>
-                                <NavLink to='/student'>Student Zone</NavLink>
-                            </li>
 
 
-                        </ul>
+            <nav className='main-nav'>
+
+                {/* --------------- Menu Links ---------------------- */}
+                <div className='menu-link'>
+                    <ul>
+                        <li>
+                            <NavLink to='/home'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/courses'>Courses</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/franchise'>Franchise</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contactus'>Contact Us</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/aboutus'>About Us</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/gallery'>Gallery</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to='/student'>Student Zone</NavLink>
+                        </li>
+
+
+                    </ul>
+                </div>
+
+
+                <div className='social-media-parent'>
+                    <div className='social-media-circle'>
+                        <div className='social-media-img-size'>
+                            <img src="/img/facebook.png" />
+                        </div>
+
                     </div>
-
-
-                    <div className='social-media-parent'>
-                        <div className='social-media-circle'>
-                            <div className='social-media-img-size'>
-                                <img src="/img/facebook.png" />
-                            </div>
-
-                        </div>
-                        <div className='social-media-circle'>
-                            <div className='social-media-img-size'>
-                                <img src="/img/instagram.png" />
-                            </div>
-                        </div>
-                        <div className='social-media-circle'>
-                            <div className='social-media-img-size'>
-                                <img src="/img/linkedin.png" />
-                            </div>
+                    <div className='social-media-circle'>
+                        <div className='social-media-img-size'>
+                            <img src="/img/instagram.png" />
                         </div>
                     </div>
+                    <div className='social-media-circle'>
+                        <div className='social-media-img-size'>
+                            <img src="/img/linkedin.png" />
+                        </div>
+                    </div>
+                </div>
 
-                </nav>
+            </nav>
 
-                {/* <section className='hero-section'>
-                    <p>Welcome to</p>
-                    <h1>Thapa Technical</h1>
 
-                </section> */}
-
-            </Grid>
         </>
     )
 }
