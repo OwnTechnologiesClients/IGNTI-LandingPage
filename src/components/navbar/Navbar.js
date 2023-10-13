1db7836cb462fc77e4e639ba79b28a05c5ce7d37
Pawan Kumar <pyadav.workmail@gmail.com>
2023-10-13 21:31:29 +0530

    dropdown navbar done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
     return (
         <>
 
-            <nav className='main-nav'>
+            <nav>
 
                 {/* --------------- Menu Links ---------------------- */}
-                <div className='menu-link'>
+                {/* <div className='menu-link'>
                     <ul>
                         <li>
                             <NavLink to='/home'>Home</NavLink>
@@ -37,29 +37,67 @@ const Navbar = () => {
 
 
                     </ul>
-                </div>
+                </div> */}
 
+                <div className='menu-parent'>
+                    <div className='menu'>
+                        <ul>
+                            <li>
+                                <NavLink to='/home'>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/courses'>Courses</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/franchise'>Franchise</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/contactus'>Contact Us</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/aboutus'>About Us</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/gallery'>Gallery</NavLink>
+                            </li>
+                            <li><NavLink to='/student'>Student Zone ↓</NavLink>
+                                <ul>
+                                    <li>
+                                        <NavLink to='/select-course'>For Exam</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to='/result'>Check Result</NavLink>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+                    <div className='social-media-parent'>
+                        <div className='social-media-circle'>
+                            <div className='social-media-img-size'>
+                                <img src="/img/facebook.png" />
+                            </div>
+
+                        </div>
+                        <div className='social-media-circle'>
+                            <div className='social-media-img-size'>
+                                <img src="/img/instagram.png" />
+                            </div>
+                        </div>
+                        <div className='social-media-circle'>
+                            <div className='social-media-img-size'>
+                                <img src="/img/linkedin.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 {/* --------------- Social media section ---------------------- */}
 
-                <div className='social-media-parent'>
-                    <div className='social-media-circle'>
-                        <div className='social-media-img-size'>
-                            <img src="/img/facebook.png" />
-                        </div>
 
-                    </div>
-                    <div className='social-media-circle'>
-                        <div className='social-media-img-size'>
-                            <img src="/img/instagram.png" />
-                        </div>
-                    </div>
-                    <div className='social-media-circle'>
-                        <div className='social-media-img-size'>
-                            <img src="/img/linkedin.png" />
-                        </div>
-                    </div>
-                </div>
 
             </nav>
 
